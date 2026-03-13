434c830d60b53b291f5ed24556552a57394e993e
Rishwanth Adhishwar <krishwanthadhiswar@gmail.com>
2026-03-13 10:23:50 +0530

    File new class added

--- Core_Java/src/fileIO/FileOutputStreamDemo.java
+++ Core_Java/src/fileIO/FileOutputStreamDemo.java
@@ -1,9 +1,25 @@
 package fileIO;
 
+import java.io.FileOutputStream;
+
 public class FileOutputStreamDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String data ="Welcome to java Programming,Happy Learning!";
+		
+		try
+		{
+			FileOutputStream output=new FileOutputStream("C:\\Users\\krish\\output.txt.txt");
+			byte[] array =data.getBytes();
+			
+			output.write(array);
+			output.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
 		
 		
 
